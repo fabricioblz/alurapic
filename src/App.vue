b32948e103b98f03d82624684fb5072677111246
Fabricio Bellesa <fabriciosilva@wine.com.br>
2017-07-25 20:41:34 -0300

    Adicionando App .vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="corpo">
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/cadastro">Cadastro</a></li>
+      </ul>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+</style>
