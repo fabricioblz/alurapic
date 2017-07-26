2a38e806ea1dc108f519acc27c38c467375841d0
Fabricio Bellesa <fabriciosilva@wine.com.br>
2017-07-25 23:28:37 -0300

    Adicionando transição ao mudar de app (página)

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
       </ul>
     </nav>
 
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -33,4 +35,14 @@
     margin: 0 auto;
     width: 96%;
   }
+
+ .pagina-enter, .pagina-leave-active {
+
+   opacity: 0;
+ }
+
+ .pagina-enter-active, .pagina-leave-active {
+
+   transition: opacity .4s;
+ }
 </style>
