245ad59b6d687002b754adcf898dbab4d1412d93
Fabricio Bellesa <fabriciosilva@wine.com.br>
2017-07-30 18:22:28 -0300

    Refatorando de $http para this.$resource

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -52,10 +52,14 @@ export default {
 
   methods: {
     save(){
-    this.$http
-    .post('v1/fotos', this.foto)
+    this.resource
+    .save(this.foto)
     .then(()=> this.foto = new Foto(), err => console.log(err));
     }
+  },
+
+  created() {
+    this.resource = this.$resource('v1/fotos/{id}')
   }
 }
 
