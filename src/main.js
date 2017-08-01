1b9f55ca35b1d419eecf6ad06249f04ce38a2a1d
Fabricio Bellesa <fabriciosilva@wine.com.br>
2017-08-01 20:42:23 -0300

    Importando vee-validate no projeto

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
+import VeeValidade from 'vee-validate';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter);
+Vue.use(VeeValidade);
 
 
 const router = new VueRouter({
