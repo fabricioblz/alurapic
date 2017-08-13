026dfd6d34346704f008f890672c27d6414d18e8
Fabricio Bellesa <fabriciosilva@wine.com.br>
2017-08-13 20:05:10 -0300

    v-bind aprendido

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -3,6 +3,18 @@
   <div>
     <h1>{{ titulo }}</h1>
 
+    <ul>
+
+        <li>
+            <img :src="foto1.url" :alt="foto1.titulo">
+        </li>
+
+        <li>
+            <img v-bind:src="foto2.url" v-bind:alt="foto2.titulo">
+        </li>
+
+    </ul>
+
   </div>
 
 </template>
@@ -12,7 +24,16 @@ export default {
 
   data() {
     return {
-      titulo: 'Alurapic'
+      titulo: 'Curso Vue',
+      foto1: {
+        url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+        titulo: 'Cachorro'
+      },
+      foto2: {
+        url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+        titulo: 'Cachorro2'
+      },
+
     }
   }
 }
@@ -20,4 +41,3 @@ export default {
 
 <style>
 </style>
-
