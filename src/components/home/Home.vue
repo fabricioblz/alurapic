4e84eafe5018b4c52e1ce8a4f7afa2a7184cca9e
Fabricio Bellesa <fabriciosilva@wine.com.br>
2017-08-13 19:56:17 -0300

    Aplicando v-bind para ensinar

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,102 +1,23 @@
 <template>
+
   <div>
+    <h1>{{ titulo }}</h1>
 
-    <h1 class="centralizado">Alurapic</h1>
-    <h3 class="centralizado">{{ mensagem }}</h3>
-
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva v-meu-transform:scale.animate='1.2' :url="foto.url" :titulo="foto.titulo" />
-          <router-link :to="{ name: 'altera', params: { id: foto._id }}"><meu-botao rotulo="ALTERAR" tipo="button"/></router-link>
-          <meu-botao rotulo='Remover' tipo='button' :confirmation='false' classStyle='danger' @buttonTrigged='remove(foto)' />
-        </meu-painel>
-      </li>
-    </ul>
   </div>
+
 </template>
 
 <script>
-
-import Painel from '../shared/painel/Painel.vue';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
-import Botao from '../shared/botao/Botao.vue'
-import transform from '../../directives/Transform';
-import FotoService from '../../domain/foto/FotoService';
-
 export default {
 
-  components: {
-    'meu-botao': Botao,
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
-  },
-
-  directives:{
-    'meu-transform': transform
-  },
-
-  data () {
+  data() {
     return {
-
-      fotos: [],
-      mensagem: '',
-
-      filtro: ''
+      titulo: 'Alurapic'
     }
-  },
-
-  methods: {
-    remove(foto){
-      this.fotoService
-      .apaga(foto._id)
-      .then(()=>{
-        let index = this.fotos.indexOf(foto);
-        this.fotos.splice(index, 1)
-
-        this.mensagem = 'Imagem removida com sucesso!'
-      }, err=>this.mensagem = err.message)
-    }
-  },
-
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-
-  created() {
-    this.fotoService = new FotoService(this.$resource)
-
-    this.fotoService
-    .lista()
-    .then(fotos => this.fotos = fotos, err => this.mensagem = err.message);
   }
 }
 </script>
+
 <style>
-
-.centralizado {
-  text-align: center;
-}
-
-.lista-fotos {
-  list-style: none;
-}
-
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
-}
 </style>
+
