fdc48adbd8d41e707c19fc816d5bd717d54b8325
Fabricio Bellesa <fabriciosilva@wine.com.br>
2017-08-14 21:02:06 -0300

    minor

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,10 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <div class="painel">
-            <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-            <transition name="painel-fade">
-                <div class="painel-conteudo">
-                  <img :src='foto.url' class='imagem-responsiva'>
-                </div>
-            </transition>
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img :src='foto.url' class='imagem-responsiva'>
+          </div>
         </div>
 
       </li>
@@ -53,73 +51,73 @@ export default {
   created() {
 
     this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 
 </script>
 
 <style>
-  .corpo {
-    font-family: Helvetica, sans-serif;
-    width: 96%;
-    margin: 0 auto;
-  }
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
 
-  .centralizado {
+.centralizado {
 
-    text-align: center;
-  }
+  text-align: center;
+}
 
-  .lista-fotos {
-    list-style: none;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos .lista-fotos-item {
+.lista-fotos .lista-fotos-item {
 
-    display: inline-block;
-  }
+  display: inline-block;
+}
 
-  .filtro {
+.filtro {
 
-    display: block;
-    width: 100%;
-  }
+  display: block;
+  width: 100%;
+}
 
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
 
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
 
- .painel-fade-enter, .painel-fade-leave-active {
+.painel-fade-enter, .painel-fade-leave-active {
 
-     opacity: 0;
- }
+  opacity: 0;
+}
 
- .painel-fade-enter-active, .painel-fade-leave-active {
+.painel-fade-enter-active, .painel-fade-leave-active {
 
-     transition: opacity .4s;
- }
+  transition: opacity .4s;
+}
 
-  .imagem-responsiva {
+.imagem-responsiva {
 
-    width: 100%;
-  }
+  width: 100%;
+}
 </style>
